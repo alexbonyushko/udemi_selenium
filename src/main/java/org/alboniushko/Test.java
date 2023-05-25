b1de77f4a2a60159deeb7a892849f2d44c8f0837
alboniushko <a.bonyushko@gmail.com>
2023-05-25 19:20:40 +0200

    24.Locators

--- src/main/java/org/alboniushko/Test.java
+++ src/main/java/org/alboniushko/Test.java
@@ -1,12 +1,16 @@
 package org.alboniushko;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","/Users/alboniushko/IdeaProjects/udemi_selenium/src/main/resources/chromedriver" );
-        WebDriver driver=new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "/Users/alboniushko/IdeaProjects/udemi_selenium/src/main/resources/chromedriver");
+        WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
+
+        WebElement input = driver.findElement(By.id("QWE"));
     }
 }
