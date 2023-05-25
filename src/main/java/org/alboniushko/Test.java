830f16ba525ef51f840c204000788768c30baaae
alboniushko <a.bonyushko@gmail.com>
2023-05-25 18:00:42 +0200

    23.Set Selenium

---
+++ src/main/java/org/alboniushko/Test.java
@@ -0,0 +1,12 @@
+package org.alboniushko;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","/Users/alboniushko/IdeaProjects/udemi_selenium/src/main/resources/chromedriver" );
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com/");
+    }
+}
