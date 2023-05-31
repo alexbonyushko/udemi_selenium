679873411af68c5560ab5cc9f857625533663e69
alboniushko <a.bonyushko@gmail.com>
2023-05-31 15:52:05 +0200

    29. Input Text

--- src/main/java/org/alboniushko/InputTextTest.java
+++ src/main/java/org/alboniushko/InputTextTest.java
@@ -1,6 +1,7 @@
 package org.alboniushko;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,19 +9,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
 /**
- * 28.Method Click
+ * 29. Input Text
  */
-public class MethodClick {
+public class InputTextTest {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/Users/alboniushko/IdeaProjects/udemi_selenium/src/main/resources/chromedriver");
         WebDriver driver = new ChromeDriver();
+
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);//неявное ожидание элемента на странице для всего проекта
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);//ожидание полной загрузки страницы
         driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);//ожидание выполнения асинхронного запроса
 
-        WebElement element = driver.findElement(By.id("qwe"));
+        By input = By.xpath("//*");
 
-        element.click();
+        WebElement element = driver.findElement(input);
+
+        element.sendKeys("автомобили", Keys.ENTER);//ввод в инпут слова автомобили и нажатие ентера
+        /**
+         * sendKeys для:
+         * 1. вставки текста и его отправки
+         * 2. передача кодов конопок ( например копировать/вставить)
+         * 3. загрузка файла на сервер
+         */
 
         driver.get("https://www.google.com/");
     }
